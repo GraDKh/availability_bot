8d5c340911fb30bd32ee1a6b21a0759fbc11d104
Dmitriy Gordon <dmitriy.gordon@ncloudtech.ru>
2017-05-08 10:56:42 +0300

    Add unit tests

--- src/main.rs
+++ src/main.rs
@@ -26,21 +26,23 @@ use telegram_bot::Api;
 
 fn main() {
     env_logger::init().unwrap_or_else(|error| {
-        let message = format!("Couldn't initialize logging {:?}", error);
-        println!("{}", message);
-        panic!("{}", message);
-    });
+                                          let message = format!("Couldn't initialize logging {:?}",
+                                                                error);
+                                          println!("{}", message);
+                                          panic!("{}", message);
+                                      });
 
     let configuration = configuration::Configuration::load();
 
-    let bot_api = Api::from_token(&configuration.bot_token).expect("Couldn't create API object for bot");
+    let bot_api =
+        Api::from_token(&configuration.bot_token).expect("Couldn't create API object for bot");
     info!("getMe: {:?}", bot_api.get_me());
 
     let mut message_sender = telegram_api::TelegramMessageSender::new(&bot_api);
     let mut events_sender = events_sender::CalendarEventsSender::new();
-    let mut data_saver = save_load_state::create_data_saver(&configuration.data_file);
+    let mut data_saver = save_load_state::FileDataSaver::new(&configuration.data_file);
     let mut message_processor = message_processor::UserCollection::new(&mut message_sender,
-                                                                   &mut events_sender,
-                                                                   &mut *data_saver);
+                                                                       &mut events_sender,
+                                                                       &mut data_saver);
     telegram_api::process_messages(&bot_api, &mut message_processor);
 }
--- src/message_processor.rs
+++ src/message_processor.rs
@@ -10,8 +10,8 @@ use std::cell::RefCell;
 use std::collections::HashMap;
 
 trait StateProcessor<State> {
-    fn process(&self, state: 
-               &mut State, 
+    fn process(&self,
+               state: &mut State,
                answer: &str,
                message_sender: &mut MessageSender,
                events_sender: &mut EventsSender);
@@ -43,12 +43,14 @@ impl<State, Processor: StateProcessor<State>> DialogStateMachine<State, Processo
         let state_holder = &mut self.state_holder.deref().borrow_mut();
         self.processor
             .deref()
-            .process(state_holder.get_state(), answer, message_sender, events_sender);
+            .process(state_holder.get_state(),
+                     answer,
+                     message_sender,
+                     events_sender);
     }
 }
 
-struct BotStateProcessor {
-}
+struct BotStateProcessor {}
 
 impl BotStateProcessor {
     fn new() -> Self {
@@ -57,32 +59,40 @@ impl BotStateProcessor {
 }
 
 impl StateProcessor<UserInfo> for BotStateProcessor {
-    fn process(&self, user_info: &mut UserInfo, answer: &str,
+    fn process(&self,
+               user_info: &mut UserInfo,
+               answer: &str,
                message_sender: &mut MessageSender,
                events_sender: &mut EventsSender) {
         match user_info.state {
             BotState::Initial => {
                 if answer.starts_with("/help") {
-                    message_sender.send_text(user_info.chat_id, "https://www.youtube.com/watch?v=yWP6Qki8mWc".to_string());
+                    message_sender.send_text(user_info.chat_id,
+                                             "https://www.youtube.com/watch?v=yWP6Qki8mWc"
+                                                 .to_string());
                     user_info.state = BotState::Initial;
                 } else if answer.starts_with("/whoami") {
                     {
-                        let calendar_name = user_info.get_calendar_name().as_ref().map(|name| name.as_str())
+                        let calendar_name = user_info
+                            .get_calendar_name()
+                            .as_ref()
+                            .map(|name| name.as_str())
                             .unwrap_or("<not specified>");
-                        let message =
-                            format!("{} {}\nIn calendar will be \"{}\"",
-                                    user_info.get_first_name(),
-                                    user_info.get_last_name(),
-                                    calendar_name);
+                        let message = format!("{} {}\nIn calendar will be \"{}\"",
+                                              user_info.get_first_name(),
+                                              user_info.get_last_name(),
+                                              calendar_name);
                         message_sender.send_text(user_info.chat_id, message);
                     }
                     user_info.state = BotState::Initial;
                 } else if answer.starts_with("/setmyname") {
-                    message_sender.send_text(user_info.chat_id, "Enter the name to be used in calendar".to_string());
+                    message_sender.send_text(user_info.chat_id,
+                                             "Enter the name to be used in calendar".to_string());
                     user_info.state = BotState::SetName
                 } else if answer.starts_with("/wfh") {
                     if user_info.get_calendar_name().is_some() {
-                        message_sender.send_menu(user_info.chat_id, "Send work from home for today?".to_string(),
+                        message_sender.send_menu(user_info.chat_id,
+                                                 "Send work from home for today?".to_string(),
                                                  vec![vec!["yes".to_string(), "no".to_string()]]);
                         user_info.state = BotState::WfhConfirmation;
                     }
@@ -94,9 +104,10 @@ impl StateProcessor<UserInfo> for BotStateProcessor {
                     user_info.state = BotState::Initial;
                     {
                         match user_info.get_calendar_name() {
-                            Some(name) => events_sender.post_wfh(name.as_str(),
-                                                                 &chrono::Local::today()),
-                            None => error!("WFH: User name for {:?} not specified", user_info)
+                            Some(name) => {
+                                events_sender.post_wfh(name.as_str(), &chrono::Local::today())
+                            }
+                            None => error!("WFH: User name for {:?} not specified", user_info),
                         }
                     }
                 } else if answer == "no" {
@@ -106,7 +117,8 @@ impl StateProcessor<UserInfo> for BotStateProcessor {
             }
             BotState::SetName => {
                 user_info.set_calendar_name(answer.to_string());
-                message_sender.send_text(user_info.chat_id, format!("Your name will be \"{}\"", answer));
+                message_sender.send_text(user_info.chat_id,
+                                         format!("Your name will be \"{}\"", answer));
                 user_info.state = BotState::Initial;
             }
         }
@@ -146,8 +158,9 @@ pub struct UserCollection<'a> {
 
 impl<'a> UserCollection<'a> {
     pub fn new(message_sender: &'a mut MessageSender,
-           events_sender: &'a mut EventsSender,
-           data_saver: &'a mut DataSaver,) -> Self {
+               events_sender: &'a mut EventsSender,
+               data_saver: &'a mut DataSaver)
+               -> Self {
         let mut result = Self {
             message_sender,
             events_sender,
@@ -169,24 +182,24 @@ impl<'a> UserCollection<'a> {
                  })
             .collect();
         let serialization_data =
-            UserCollectionSerializationData::new(self.last_message_id.expect("last_message_id not specified but save is invoked"), users);
+            UserCollectionSerializationData::new(
+                self.last_message_id.expect("last_message_id not specified but save is invoked"), users);
         match self.data_saver.save_data(serialization_data) {
-            Ok(_) => {},
-            Err(error) => error!("Couldn't save bot state: {}", error.description())
+            Ok(_) => {}
+            Err(error) => error!("Couldn't save bot state: {}", error.description()),
         };
     }
 
     fn load(&mut self) {
         match self.data_saver.load_data() {
             Ok(user_data) => {
-                     self.last_message_id = Some(user_data.last_id);
-                     for user_info in user_data.users {
-                         self.users
-                             .insert(user_info.chat_id,
-                                     User::new(user_info.info));
-                     }
-                 }
-            Err(error) => warn!("Couldn't load bot state: {:?}", error.description())
+                self.last_message_id = Some(user_data.last_id);
+                for user_info in user_data.users {
+                    self.users
+                        .insert(user_info.chat_id, User::new(user_info.info));
+                }
+            }
+            Err(error) => warn!("Couldn't load bot state: {:?}", error.description()),
         }
     }
 }
@@ -201,21 +214,22 @@ impl<'a> MessageProcessor for UserCollection<'a> {
             (first_name.to_string(),
              match last_name {
                  Some(name) => name.to_string(),
-                 None => "".to_string()
+                 None => "".to_string(),
              })
         };
 
         {
             let mut user = self.users
-                    .entry(chat_id)
-                    .or_insert_with(|| {
-                                        let (first_name, last_name) = get_name();
-                                        User::new(UserInfo::new(chat_id,
-                                                                BotState::Initial,
-                                                                first_name,
-                                                                last_name))
-                                    });
-            user.state_machine.process(message, self.message_sender, self.events_sender);
+                .entry(chat_id)
+                .or_insert_with(|| {
+                                    let (first_name, last_name) = get_name();
+                                    User::new(UserInfo::new(chat_id,
+                                                            BotState::Initial,
+                                                            first_name,
+                                                            last_name))
+                                });
+            user.state_machine
+                .process(message, self.message_sender, self.events_sender);
         }
 
         self.save();
@@ -237,5 +251,123 @@ impl<'a> MessageProcessor for UserCollection<'a> {
         }
                };
     }
-    
+}
+
+#[cfg(test)]
+mod tests {
+
+use ::basic_structures::*;
+use ::user_data::*;
+use ::save_load_state::{DataSaver, SaveResult, LoadResult, UserCollectionSerializationData, UserSerializationInfo};
+
+use std::cell::Cell;
+use std::borrow::BorrowMut;
+use std::ops::Deref;
+
+enum Event {
+    WfhSingleDay{name: String, date: LocalDate},
+}
+
+struct MockEventsSender {
+    events: Vec<Event>
+}
+
+impl MockEventsSender {
+    fn new() -> Self {
+        Self {events: Vec::<Event>::new()}
+    }
+}
+
+impl EventsSender for MockEventsSender {
+    fn post_wfh(&mut self, name: &str, date: &LocalDate) {
+        self.events.push(Event::WfhSingleDay{name: name.to_string(), date: date.clone()});
+    }
+}
+
+type Menu = Vec<Vec<String>>;
+
+#[derive(Eq, PartialEq, Debug)]
+struct Message {
+    chat_id: ChatID,
+    text: String,
+    menu: Option<Menu>
+}
+
+impl Message {
+    fn new<S>(chat_id: ChatID, text: S, menu: Option<Menu>) -> Self
+        where S: Into<String> {
+        Self {chat_id, text: text.into(), menu}
+    }
+}
+
+struct MockMessageSender {
+    messages: Vec<Message>
+}
+
+impl MockMessageSender {
+    fn new() -> Self {
+        Self {messages: Vec::<Message>::new()}
+    }
+}
+
+impl MessageSender for MockMessageSender {
+    fn send_text(&mut self, chat_id: ChatID, text: String) {
+        self.messages.push(Message::new(chat_id, text, None));
+    }
+
+    fn send_menu(&mut self, chat_id: ChatID, text: String, menu: Vec<Vec<String>>) {
+        self.messages.push(Message::new(chat_id, text, Some(menu)));
+    }
+}
+
+struct MockDataSaver {
+    save_count: Cell<i32>,
+    load_count: Cell<i32>
+}
+
+impl MockDataSaver {
+    fn new() -> Self {
+        Self{save_count: Cell::new(0), load_count: Cell::new(0)}
+    }
+}
+
+impl DataSaver for MockDataSaver {
+    fn save_data(&self, data: UserCollectionSerializationData) -> SaveResult {
+        self.save_count.set(self.save_count.get() + 1);
+        Ok(())
+    }
+
+    fn load_data(&self) -> LoadResult {
+        self.load_count.set(self.load_count.get() + 1);
+        Ok(UserCollectionSerializationData::new(42, Vec::<UserSerializationInfo>::new()))
+    }
+}
+
+fn simple_reply_test(first_name: &str, last_name: Option<&str>, text: &str, expected_reply: &str) {
+    let mut message_sender = MockMessageSender::new();
+    let mut events_sender = MockEventsSender::new();
+    let mut data_saver = MockDataSaver::new();
+    {
+        let mut message_processor = super::UserCollection::new(&mut message_sender,
+                                                    &mut events_sender,
+                                                    &mut data_saver);
+        
+        message_processor.process_message(42, first_name, last_name, text);
+    }
+    assert_eq!(events_sender.events.len(), 0);
+    assert_eq!(data_saver.save_count, Cell::new(1));
+    assert_eq!(message_sender.messages, vec![Message::new(42, expected_reply, None)]);
+}
+
+#[test]
+fn test_help() {
+    simple_reply_test("Vasiliy", None, "/help", "https://www.youtube.com/watch?v=yWP6Qki8mWc");
+}
+
+#[test]
+fn test_whoami() {
+    simple_reply_test("Vasiliy", None, "/whoami", "Vasiliy \nIn calendar will be \"<not specified>\"");
+    simple_reply_test("Vasiliy", Some("Pupkin"), "/whoami", "Vasiliy Pupkin\nIn calendar will be \"V.Pupkin\"");
+}
+
 }
